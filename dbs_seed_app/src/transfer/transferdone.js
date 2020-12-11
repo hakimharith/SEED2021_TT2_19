8bedf39f6d8bededb26001db9aedbb12da399b8c
LeeJiaWeiNTU <JLEE224@e.ntu.edu.sg>
2020-12-11 16:06:37 +0800

    Route

--- dbs_seed_app/src/transfer/transferdone.js
+++ dbs_seed_app/src/transfer/transferdone.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-
+import {Route, BrowserRouter as Router} from "react-router-dom";
 import { Col, Divider, Row, } from 'antd';
 import 'antd/dist/antd.css';
 
